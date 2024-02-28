8880fbbf9b1078f96a2a326c7f6cf9772b432031
= <sipher@creepy.sh>
2024-02-28 17:55:27 -0600

    Uhh i have no comment..

---
+++ static/js/logout.js
@@ -0,0 +1,14 @@
+// logout.js
+$(document).ready(function () {
+    $('.logout-btn').on('click', function (e) {
+        console.log(1);
+        $.post('/logout', { _token: $('input[name=_token]').val() }, function (resp) {
+            if (resp.status == "done") {
+                location.reload();
+            } else {
+                alert(resp.msg);
+            }
+        });
+        e.preventDefault();
+    });
+});
